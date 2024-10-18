70bf2e27618fd7b95c17d8b73fa0148f0e6fd84c
Wasabi-Hg <164887597+Wasabi-Hg@users.noreply.github.com>
2024-10-17 21:31:20 -0500

    Delete puts1.cpp

--- puts1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <stdio.h>
-using namespace std;
-
-int main() {
-    int val = puts("Hello World!");
-    cout << "The value returned from puts function is: " << val << endl;
-    return 0;
-}
